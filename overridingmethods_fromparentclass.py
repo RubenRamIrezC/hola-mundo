8c3ced71561677c14f966c5d18920dc474ace0eb
RubenRamirez <ruben_uo19@outlook.com>
2022-02-05 17:50:52 -0600

    agregué archivo

---
+++ overridingmethods_fromparentclass.py
@@ -0,0 +1,3 @@
+# Say the class Car had a method called fill_gas_tank(). This method is meaningless 
+# for an all-electric vehicle, so you might want to override this method. Here´s  one way
+# to do that:
